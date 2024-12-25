1cd337e43f8d4be5bcc8b9ccccb32e74e03f24ef
Aayan <128874715+MUIabk@users.noreply.github.com>
2024-12-25 17:54:07 +0530

    Update wavelet.cpp

--- wavelet.cpp
+++ wavelet.cpp
@@ -1,54 +1,75 @@
-int a[N];
-struct wavelet_tree{    // O(nlogA) space, O(logA) time, 1-based
-	#define vi vector<int>
-	#define pb push_back
-	int lo, hi;   // use compression for logA -> logn
-	wavelet_tree *l, *r;
-	vi b;
-	//nos are in range [x,y]
-	//array indices are [from, to)
-	wavelet_tree(int *from, int *to, int x, int y){
-		lo = x, hi = y;
-		if(lo == hi or from >= to) return;
-		int mid = (lo+hi)/2;
-		auto f = [mid](int x){
-			return x <= mid;
-		};
-		b.reserve(to-from+1);
-		b.pb(0);
-		for(auto it = from; it != to; it++)
-			b.pb(b.back() + f(*it));
-		auto pivot = stable_partition(from, to, f);
-		l = new wavelet_tree(from, pivot, lo, mid);
-		r = new wavelet_tree(pivot, to, mid+1, hi);
-	}
-	//kth smallest element in [l, r]
-	int kth(int l, int r, int k){
-		if(l > r) return 0;
-		if(lo == hi) return lo;
-		int inLeft = b[r] - b[l-1];
-		int lb = b[l-1]; //amt of nos in first (l-1) nos that go in left 
-		int rb = b[r]; //amt of nos in first (r) nos that go in left
-		if(k <= inLeft) return this->l->kth(lb+1, rb , k);
-		return this->r->kth(l-lb, r-rb, k-inLeft);
-	}
-	//count of nos in [l, r] Less than or equal to k
-	int LTE(int l, int r, int k) {
-		if(l > r or k < lo) return 0;
-		if(hi <= k) return r - l + 1;
-		int lb = b[l-1], rb = b[r];
-		return this->l->LTE(lb+1, rb, k) + this->r->LTE(l-lb, r-rb, k);
-	}
-	//count of nos in [l, r] equal to k
-	int count(int l, int r, int k) {
-		if(l > r or k < lo or k > hi) return 0;
-		if(lo == hi) return r - l + 1;
-		int lb = b[l-1], rb = b[r], mid = (lo+hi)/2;
-		if(k <= mid) return this->l->count(lb+1, rb, k);
-		return this->r->count(l-lb, r-rb, k);
-	}
-	~wavelet_tree(){
-		delete l;
-		delete r;
-	}
+struct wavelet{
+    struct Info{
+        int st, lc, rc;
+        Info():Info(0,-1,-1){}
+        Info(int st, int lc, int rc){
+            this->st = st;
+            this->lc = lc;
+            this->rc = rc;
+        }
+    };
+    int mn = inf, mx = 0;
+    int n;
+    vector<int> p;
+    vector<Info> wt;
+    vector<int> a;
+    //its 0-based
+    //space upper bound : n * log (mxe(a))
+    //query : log(mxe(a))
+    //build : n * log (mxe(a))
+    //to reduce log(mxe(a)) to log(n) perform coordinate compression 
+    wavelet(vector<int>&a){
+        n = sz(a);
+        for(auto &x : a){
+            mx = max(mx, x);
+            mn = min(mn, x);
+        }
+        this->a = a;
+        build(this->a.begin(), this->a.end(), mn, mx);
+    }
+    template<typename IT>
+    void build(IT from, IT to, int lo, int hi){
+        int m = hi + lo >> 1;
+        auto f = [&](int x){ return x <= m; };
+        p.pb(0);
+        int i = sz(wt);
+        wt.push_back(Info());
+        wt[i].st = sz(p);
+        for(auto it = from; it != to; ++it) {
+            p.push_back(p.back() + f(*it));
+        }
+        if(lo == hi or from == to)return;
+        auto it = stable_partition(from, to, f);
+        wt[i].lc = sz(wt);
+        build(from, it, lo, m);
+        wt[i].rc = sz(wt);
+        build(it, to, m + 1, hi);
+    }
+    int equals(int i, int l, int r, int val, int lo, int hi){
+        if(l > r)return 0;
+        if(lo == hi){
+            if(lo == val)return p[wt[i].st + r] - p[wt[i].st + l - 1];
+            return 0;
+        }
+        int m = hi + lo >> 1;
+        int lp = p[wt[i].st + l - 1], rp = p[wt[i].st + r];
+        if(val <= m)return equals(wt[i].lc, lp, rp - 1, val, lo, m); 
+        return equals(wt[i].rc, l - lp, r - rp , val, m + 1, hi);
+    }
+    int less_equals(int i, int l, int r, int val, int lo, int hi){
+        if(l > r or lo > val)return 0;
+        if(hi <= val)return r - l + 1;
+        int m = hi + lo >> 1;
+        int lp = p[wt[i].st + l - 1], rp = p[wt[i].st + r];
+        return less_equals(wt[i].lc, lp, rp - 1, val, lo, m) + less_equals(wt[i].rc, l - lp, r - rp , val, m + 1, hi);
+    }
+    // 1 - based
+    int kth(int i, int l, int r, int val, int lo, int hi){
+        if(lo == hi)return lo;
+        int m = hi + lo >> 1;
+        int lp = p[wt[i].st + l - 1], rp = p[wt[i].st + r];
+        int left = rp - lp;
+        if(left >= val)return kth(wt[i].lc, lp, rp - 1, val, lo, m);
+        return kth(wt[i].rc, l - lp, r - rp , val - left, m + 1, hi);
+    }
 };
